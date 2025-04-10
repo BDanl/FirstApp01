d2b09043009aa34199b590f359a9d96f98fb80cf
BDanl <alejandrobovea@gmail.com>
2025-04-09 21:32:27 -0500

    idcrud1st

--- screens/TomatoScreen.js
+++ screens/TomatoScreen.js
@@ -8,8 +8,11 @@ import BottomNavigationBar from "../components/BottomNavigationBar";
 import { TextInput } from "react-native-gesture-handler";
 import React from "react";
 import { useEffect } from "react";
-import { collection, getDoc, getDocs, doc, setDoc, addDoc, updateDoc, deleteDoc  } from "firebase/firestore";
+import { collection, getDoc, getDocs, doc, setDoc, addDoc, updateDoc, deleteDoc,query, where, documentId  } from "firebase/firestore";
 import { db } from "../firebaseConfig";
+import { getFirestore } from 'firebase/firestore';
+
+
  
 
 const TomatoScreen = () => {
@@ -41,50 +44,61 @@ const TomatoScreen = () => {
     setDishes(dishesList);
   };
   
-  const addData = async () => {
-    await addDoc(collection(db, "foods"), {
-      title: titleInput,
-      price: parseInt(priceInput)
-     });
-     
-  }
+ const addData = async () => {
+   const docRef = await addDoc(collection(db, "foods"), {
+     title: titleInput,
+     price: parseInt(priceInput)
+   });
+   
+   console.log(`Documento agregado con éxito con ID: ${docRef.id}`);
+ }
   const updateData = async () => {
-    await updateDoc(doc(db, "foods", "21qPfhokIUVF1ZuSxUdm"), {
+    title = titleInput;
+    const docId = await getId(title);
+    await updateDoc(doc(db, "foods", docId), {
       title: titleInput,
       price: parseInt(priceInput)
      });
   }
 
   const deleteData = async () => {
-    getAllData();
-    const title = titleInput; 
-    const querySnapshot = await db.collection("foods").where("title", "==", title).get();
-    const docRef = querySnapshot.docs[0];
-    if (docRef) {
-      await deleteDoc(doc(db, "foods", docRef.id)); o
-    } else {
-      console.log("No se encontró el documento");
-    }
-  };
-  const getId = async () => {
-  const db = firebase.firestore();
-  const collectionRef = db.collection("foods");
+    title = titleInput;
+    const docId = await getId(title);
+    await deleteDoc(doc(db, "foods", docId));
 
-  const querySnapshot = await collectionRef.getDocs();
-  querySnapshot.forEach((doc) => {
-  const datos = doc.data();
-  if (datos.title === titleInput) {
-    console.log(doc.id); // Imprime el ID del documento
+  };
+ 
+  const getId = async (titleid) => {
+    const querySnapshot = await getDocs(collection(db, "foods"));
+    for (const doc of querySnapshot.docs) {
+      if (doc.data().title === titleid) {
+        console.log("ID: ", doc.id, " => ", doc.data());
+        return doc.id;
+      }
+    }
+    console.log("No se encontró el documento");
   }
+
+  const getMultipleData = async () => {
+    const q = query(collection(db, "foods"), where("price", "==", 2000));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
 });
   }
-  const choto = (title, price) => {
-    /* console.log(title);
-    setTitle(title);
-    console.log(price);
-    setPrice(price); */
-    
+
+  const matchTitle = async () => {
+    for (const dish of dishes) {
+      if (dish.title === titleInput) {
+        const h = dish.title;
+        console.log("TiltleDoc: ", h);
+        return h;
+      }
+    }
+    console.log("No se encontró el documento");
   }
+  
   useEffect(() => {
     getAllData();
     getData();
@@ -120,21 +134,28 @@ const TomatoScreen = () => {
       <TextInput
         style={styles.textinput}
         value={titleInput}
-        onChangeText={(text) => setTitleInput(text)}
+        onChangeText={(text) => {
+          setTitleInput(text);
+          console.log("Título ingresado:", text);
+        }}
         placeholder="Ingrese el título"
       ></TextInput>
 
       <TextInput
         style={styles.textinput}
         value={priceInput}
-        onChangeText={(text) => setPriceInput(text)}
+        onChangeText={(text) => {
+          setPriceInput(text);
+          console.log("Precio ingresado:", text);
+        }}
         placeholder="Ingrese el precio"
         keyboardType="numeric"
       ></TextInput>
       <Button title="Add data" onPress={() => addData()}/>
       <Button title="Update data" onPress={() => updateData()}/>
       <Button title="Delete data" onPress={() => deleteData()}/>
-      <Button title="Get ID" onPress={() => getId()}/>
+      <Button title="get  multiple data" onPress={() => getMultipleData()}/>
+      <Button title="get id" onPress={() => getId()}/>
       <Text
         style={styles.text}
         onPress={() => navigation.navigate("PurpleScreen" , {
@@ -152,7 +173,8 @@ const TomatoScreen = () => {
           <Text key={index}>
             {/* index===0 *//* dish.title.includes("Jopo") */ dish.price>1000 && (
               <Text>
-                {dish.title + ": "}
+                {dish.title + ": " }
+                
             
                 {dish.price + " "}
                 {'\n'}
